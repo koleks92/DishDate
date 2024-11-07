53e50b7138e1706ac1a165ccd342b7ec826c5e08
koleks92 <koleks92@gmail.com>
2024-11-07 19:40:35 +0100

    fix: Removed app from imports

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { app } from "./services/firebaseConfig";
 
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
