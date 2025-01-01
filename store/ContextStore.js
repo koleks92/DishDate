cd43baab6500c12e37dbdd99f215c2188da39de8
koleks92 <koleks92@gmail.com>
2025-01-01 18:26:26 +0100

    feat: Load Firebase

--- store/ContextStore.js
+++ store/ContextStore.js
@@ -1,10 +1,13 @@
 import { createContext, useState } from "react";
 import auth from "@react-native-firebase/auth";
+import firestore from '@react-native-firebase/firestore';
+
 
 export const DDContext = createContext();
 
 export const DDProvider = ({ children }) => {
     const [user, setUser] = useState(user);
+    const [dishes, setDishes] = useState(null);
 
     // Handle SignOut
     const handleSignOut = () => {
@@ -14,8 +17,25 @@ export const DDProvider = ({ children }) => {
         setUser(null)
     };
 
+    // Get Dishes from the database
+    const loadDishesHandler = async () => {
+        try {
+            console.log("Loading dishes from Firestore...");
+            const dishesCollection = await firestore().collection('Dishes').get();
+    
+            // Process the dishes
+            const dishes = dishesCollection.docs.map(doc => ({
+                id: doc.id,  // Document ID
+                ...doc.data() // Document data
+            }));
+    
+            console.log("Dishes:", dishes);
+        } catch (error) {
+            console.error("Error loading dishes from Firestore:", error);
+        }
+    }
     return (
-        <DDContext.Provider value={{ user, setUser, handleSignOut }}>
+        <DDContext.Provider value={{ user, setUser, handleSignOut, loadDishesHandler, dishes }}>
             {children}
         </DDContext.Provider>
     );
