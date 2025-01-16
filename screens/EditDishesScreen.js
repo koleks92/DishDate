d6dd9724d7823cc19d989d534f9d6cd894dc295f
koleks92 <koleks92@gmail.com>
2025-01-16 22:01:05 +0100

    feat: pickImage from the gallery and image preview

--- screens/EditDishesScreen.js
+++ screens/EditDishesScreen.js
@@ -1,10 +1,11 @@
-import { Text, View, StyleSheet, TextInput, Button } from "react-native";
-
+import { Text, View, StyleSheet, TextInput, Button, Image } from "react-native";
+import * as ImagePicker from 'expo-image-picker';
 import { useState } from "react";
 
 function EditDishesScreen({ route }) {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
+    const [image, setImage] = useState('');
 
     // Get edit form the route
     const { edit } = route.params || {};
@@ -13,6 +14,28 @@ function EditDishesScreen({ route }) {
     const saveDish = () => {
         console.log("Save TODO")
     }
+
+    // Take picture handler
+    const takePictureHandler = async () => {
+        console.log("TODO")
+    }
+
+    // Pick image from the library handler
+    const pickImageHandler = async () => {
+        
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ['images', 'videos'],
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+    
+        console.log(result);
+    
+        if (!result.canceled) {
+          setImage(result.assets[0].uri);
+        }
+      };
 
     if (edit) {
         return (
@@ -27,6 +50,10 @@ function EditDishesScreen({ route }) {
                 <Text>New Dish</Text>
                 <TextInput value={name} onChangeText={setName} placeholder="Enter name" />
                 <TextInput value={description} onChangeText={setDescription} placeholder="Enter description" />
+                <Button title="Pick an image from camera roll" onPress={pickImageHandler} />
+                <Button title="Take a picture" onPress={takePictureHandler} />
+                {image && <Image source={{ uri: image }} style={styles.image} />} 
+
                 <Button title="Save" onPress={() => { saveDish }} />
 
             </View>
@@ -42,5 +69,9 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    image: {
+        width: "50%",
+        height: "50%"
     }
 })
