bcdaf347f39c4750fdea6163fc0f11a01f409dad
Jan Konieczek <koleks92@gmail.com>
2025-02-05 12:55:19 +0100

    feat: Select dishes dropdown

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,21 +2,28 @@ import { useState, useContext, useEffect, useRef } from "react";
 import { Text, View, StyleSheet, Button, Alert } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import { DDContext } from "../store/ContextStore";
+import SelectDropdown from "react-native-select-dropdown";
 
-// Finish getting the user dishes from the database
 // Select dishes for the game
 // Create a new game with the selected dishes
 
 function StartGameScreen({ navigation }) {
     const [choice, setChoice] = useState(null);
-    const [numOfDishes, setNumOfDishes] = useState();
+    const [numOfDishes, setNumOfDishes] = useState(0);
     const [selectedDishes, setSelectedDishes] = useState();
+    const [selectedDishesChoice, setSelectedDishesChoice] = useState([]);
+    const [disabledIndexes, setDisabledIndexes] = useState([]);
 
     const [isLoading, setIsLoading] = useState(false);
     const [userDishes, setUserDishes] = useState([]);
 
     const { loadUserDishes, dishes } = useContext(DDContext);
 
+    let dishesDatabaseChoice = [
+        { title: "Standard Dishes", choice: 0},
+        { title: "My Dishes", choice: 1},
+        { title: "Mix Dishes", choice: 2},
+    ];
 
     const newGameHandler = () => {
         setChoice(1);
@@ -32,19 +39,28 @@ function StartGameScreen({ navigation }) {
         }
     }, [choice]);
 
+    useEffect(() => {
+        if (selectedDishesChoice == 0) {
+            setSelectedDishes(dishes);
+        } else if (selectedDishesChoice == 1) {
+            setSelectedDishes(userDishes);
+        } else {
+            setSelectedDishes(dishes.concat(userDishes));
+        }
+    }, [selectedDishesChoice]);
+
     // Get user dishes from the database
     const getUserDishes = async () => {
         setIsLoading(true);
         const data = await loadUserDishes();
 
-        // Check if `userDishes` is empty after loading
+        // Set disabled if no user dishes or less than 5 user dishes
         if (!data || data.length === 0) {
-            Alert.alert(
-                "Error",
-                "There are no dishes in the database. Please add some."
-            );
-            navigation.goBack();
+            setDisabledIndexes([1, 2]);
+        } else if (data.length < 5) {
+            setDisabledIndexes([1]);
         }
+
         setUserDishes(data);
         setIsLoading(false);
     };
@@ -93,6 +109,46 @@ function StartGameScreen({ navigation }) {
                         onChangeText={setNumOfDishes}
                         placeholder="Enter number of dishes ( 5 - 25 )"
                     />
+                    <SelectDropdown
+                        data={dishesDatabaseChoice}
+                        disabledIndexes={disabledIndexes}
+                        onSelect={(selectedItem, index) => {
+                            setSelectedDishesChoice(selectedItem.choice);
+                        }}
+                        renderButton={(selectedItem) => {
+                            return (
+                                <View style={styles.dropdownButtonStyle}>
+                                    <Text style={styles.dropdownButtonTxtStyle}>
+                                        {(selectedItem && selectedItem.title) ||
+                                            "Select Dishes"}
+                                    </Text>
+                                </View>
+                            );
+                        }}
+                        renderItem={(item, index, isSelected) => {
+                            console.log(item); // Check the value of item.disabled
+
+                            return (
+                                <View
+                                    style={{
+                                        ...styles.dropdownItemStyle,
+                                        ...(isSelected && {
+                                            backgroundColor: "#D2D9DF",
+                                        }),
+                                        ...(item.disabled && {
+                                            backgroundColor: "#CCCCCC",
+                                        }),
+                                    }}
+                                >
+                                    <Text style={styles.dropdownItemTxtStyle}>
+                                        {item.title}
+                                    </Text>
+                                </View>
+                            );
+                        }}
+                        showsVerticalScrollIndicator={false}
+                        dropdownStyle={styles.dropdownMenuStyle}
+                    />
                     <Button title="Start Game" onPress={createNewGameHandler} />
                 </View>
             </View>
@@ -116,5 +172,50 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+    },
+    dropdownButtonStyle: {
+        width: 200,
+        height: 50,
+        backgroundColor: "#E9ECEF",
+        borderRadius: 12,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: 12,
+    },
+    dropdownButtonTxtStyle: {
+        flex: 1,
+        fontSize: 18,
+        fontWeight: "500",
+        color: "#151E26",
+    },
+    dropdownButtonArrowStyle: {
+        fontSize: 28,
+    },
+    dropdownButtonIconStyle: {
+        fontSize: 28,
+        marginRight: 8,
+    },
+    dropdownMenuStyle: {
+        backgroundColor: "#E9ECEF",
+        borderRadius: 8,
+    },
+    dropdownItemStyle: {
+        width: "100%",
+        flexDirection: "row",
+        paddingHorizontal: 12,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingVertical: 8,
+    },
+    dropdownItemTxtStyle: {
+        flex: 1,
+        fontSize: 18,
+        fontWeight: "500",
+        color: "#151E26",
+    },
+    dropdownItemIconStyle: {
+        fontSize: 28,
+        marginRight: 8,
     },
 });
