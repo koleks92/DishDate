1a888f1124addafd2be74b61d0b8d407a5402f59
Jan Konieczek <koleks92@gmail.com>
2025-02-05 19:29:12 +0100

    feat: Cuisines List component

---
+++ components/CuisinesList.js
@@ -0,0 +1,81 @@
+import { View, Text, StyleSheet } from "react-native";
+import SelectDropdown from 'react-native-select-dropdown'
+
+function CuisinesList({ cuisinesList, selectedCuisine }) {
+    return (
+        <SelectDropdown
+    data={cuisinesList}
+    onSelect={(selectedItem, index) => {
+        selectedCuisine(selectedItem);
+    }}
+    renderButton={(selectedItem) => {
+      return (
+        <View style={styles.dropdownButtonStyle}>
+          <Text style={styles.dropdownButtonTxtStyle}>
+            {(selectedItem && selectedItem.name) || 'Select cuisine'}
+          </Text>
+        </View>
+      );
+    }}
+    renderItem={(item, isSelected) => {
+      return (
+        <View style={{...styles.dropdownItemStyle, ...(isSelected && {backgroundColor: '#D2D9DF'})}}>
+          <Text style={styles.dropdownItemTxtStyle}>{item.name}</Text>
+        </View>
+      );
+    }}
+    showsVerticalScrollIndicator={false}
+    dropdownStyle={styles.dropdownMenuStyle}
+  />
+    )
+}
+
+export default CuisinesList;
+
+const styles = StyleSheet.create({
+    dropdownButtonStyle: {
+      width: 200,
+      height: 50,
+      backgroundColor: '#E9ECEF',
+      borderRadius: 12,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingHorizontal: 12,
+    },
+    dropdownButtonTxtStyle: {
+      flex: 1,
+      fontSize: 18,
+      fontWeight: '500',
+      color: '#151E26',
+    },
+    dropdownButtonArrowStyle: {
+      fontSize: 28,
+    },
+    dropdownButtonIconStyle: {
+      fontSize: 28,
+      marginRight: 8,
+    },
+    dropdownMenuStyle: {
+      backgroundColor: '#E9ECEF',
+      borderRadius: 8,
+    },
+    dropdownItemStyle: {
+      width: '100%',
+      flexDirection: 'row',
+      paddingHorizontal: 12,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingVertical: 8,
+    },
+    dropdownItemTxtStyle: {
+      flex: 1,
+      fontSize: 18,
+      fontWeight: '500',
+      color: '#151E26',
+    },
+    dropdownItemIconStyle: {
+      fontSize: 28,
+      marginRight: 8,
+    },
+  });
