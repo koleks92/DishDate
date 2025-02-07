3e9329e25e4eba44dd076502f0ae8a226a7a947d
Jan Konieczek <koleks92@gmail.com>
2025-02-07 13:57:44 +0100

    feat: Moved to react-native-element-dropdown and add multiselect

--- components/CuisinesList.js
+++ components/CuisinesList.js
@@ -1,82 +1,150 @@
-import { View, Text, StyleSheet } from "react-native";
-import SelectDropdown from 'react-native-select-dropdown'
+import { Dropdown, MultiSelect } from "react-native-element-dropdown";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
 
-function CuisinesList({ cuisinesList, selectedCuisineHandler, selectedCuisine }) {
-    return (
-        <SelectDropdown
-    data={cuisinesList}
-    onSelect={(selectedItem, index) => {
-        selectedCuisineHandler(selectedItem);
-    }}
-    defaultValue={selectedCuisine}
-    renderButton={(selectedItem) => {
-      return (
-        <View style={styles.dropdownButtonStyle}>
-          <Text style={styles.dropdownButtonTxtStyle}>
-            {(selectedItem && selectedItem.name) || 'Select cuisine'}
-          </Text>
-        </View>
-      );
-    }}
-    renderItem={(item, isSelected) => {
-      return (
-        <View style={{...styles.dropdownItemStyle, ...(isSelected && {backgroundColor: '#D2D9DF'})}}>
-          <Text style={styles.dropdownItemTxtStyle}>{item.name}</Text>
-        </View>
-      );
-    }}
-    showsVerticalScrollIndicator={false}
-    dropdownStyle={styles.dropdownMenuStyle}
-  />
-    )
+function CuisinesList({
+    cuisinesList,
+    selectedCuisineHandler,
+    selectedCuisine,
+    multiselect,
+}) {
+    const [value, setValue] = useState(null);
+    const [isFocus, setIsFocus] = useState(false);
+    const [selected, setSelected] = useState([]);
+
+    useEffect(() => {
+        if (selectedCuisine) {
+            setValue(selectedCuisine);
+        }
+    }, [selectedCuisine]);
+
+    useEffect(() => {
+        if (selected.length > 0) {
+            selectedCuisineHandler(cuisinesList.filter((cuisine) => selected.includes(cuisine.id)));
+        }
+    }, [selected])
+
+
+    if (multiselect) {
+        return (
+            <View style={styles.container}>
+                <MultiSelect
+                    style={styles.dropdown}
+                    placeholderStyle={styles.placeholderStyle}
+                    selectedTextStyle={styles.selectedTextStyle}
+                    inputSearchStyle={styles.inputSearchStyle}
+                    iconStyle={styles.iconStyle}
+                    search
+                    data={cuisinesList}
+                    labelField="name"
+                    valueField="id"
+                    placeholder="Select cuisines"
+                    searchPlaceholder="Search..."
+                    maxSelect={6}
+                    value={selected}
+                    onChange={(item) => {
+                        console.log(item);
+                        setSelected(item);
+                    }}
+                    selectedStyle={styles.selectedStyle}
+                />
+            </View>
+        );
+    } else {
+        return (
+          <View style={styles.container}>
+          <MultiSelect
+              style={styles.dropdown}
+              placeholderStyle={styles.placeholderStyle}
+              selectedTextStyle={styles.selectedTextStyle}
+              inputSearchStyle={styles.inputSearchStyle}
+              iconStyle={styles.iconStyle}
+              search
+              data={cuisinesList}
+              labelField="name"
+              valueField="id"
+              placeholder="Select cuisines"
+              searchPlaceholder="Search..."
+              maxSelect={6}
+              value={selected} // Pass only the IDs to the MultiSelect
+              onChange={(item) => {              
+                  setSelected(item); // Update the state with the full items
+              }}
+              selectedStyle={styles.selectedStyle}
+          />
+      </View>
+        );
+    }
 }
 
 export default CuisinesList;
 
 const styles = StyleSheet.create({
-    dropdownButtonStyle: {
-      width: 200,
-      height: 50,
-      backgroundColor: '#E9ECEF',
-      borderRadius: 12,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      paddingHorizontal: 12,
+    container: {
+        backgroundColor: "white",
+        padding: 16,
     },
-    dropdownButtonTxtStyle: {
-      flex: 1,
-      fontSize: 18,
-      fontWeight: '500',
-      color: '#151E26',
+    dropdown: {
+        height: 50,
+        borderColor: "gray",
+        borderWidth: 0.5,
+        borderRadius: 8,
+        paddingHorizontal: 8,
     },
-    dropdownButtonArrowStyle: {
-      fontSize: 28,
+    icon: {
+        marginRight: 5,
     },
-    dropdownButtonIconStyle: {
-      fontSize: 28,
-      marginRight: 8,
+    label: {
+        position: "absolute",
+        backgroundColor: "white",
+        left: 22,
+        top: 8,
+        zIndex: 999,
+        paddingHorizontal: 8,
+        fontSize: 14,
     },
-    dropdownMenuStyle: {
-      backgroundColor: '#E9ECEF',
-      borderRadius: 8,
+    placeholderStyle: {
+        fontSize: 16,
     },
-    dropdownItemStyle: {
-      width: '100%',
-      flexDirection: 'row',
-      paddingHorizontal: 12,
-      justifyContent: 'center',
-      alignItems: 'center',
-      paddingVertical: 8,
+    selectedTextStyle: {
+        fontSize: 16,
     },
-    dropdownItemTxtStyle: {
-      flex: 1,
-      fontSize: 18,
-      fontWeight: '500',
-      color: '#151E26',
+    iconStyle: {
+        width: 20,
+        height: 20,
     },
-    dropdownItemIconStyle: {
-      fontSize: 28,
-      marginRight: 8,
+    inputSearchStyle: {
+        height: 40,
+        fontSize: 16,
     },
-  });
+    item: {
+        padding: 17,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    selectedStyle: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 14,
+        backgroundColor: "white",
+        shadowColor: "#000",
+        marginTop: 8,
+        marginRight: 12,
+        paddingHorizontal: 12,
+        paddingVertical: 8,
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+
+        elevation: 2,
+    },
+    textSelectedStyle: {
+        marginRight: 5,
+        fontSize: 16,
+    },
+});
