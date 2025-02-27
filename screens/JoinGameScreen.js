2ae0878b111331d11e662610cc65a910c92d8097
Jan Konieczek <koleks92@gmail.com>
2025-02-27 11:59:40 +0100

    feat: isSixDigits, databaseCheckGameId and navigation

--- screens/JoinGameScreen.js
+++ screens/JoinGameScreen.js
@@ -1,9 +1,38 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useContext, useState } from "react";
+import { Text, View, StyleSheet, TextInput, Button } from "react-native";
+import { DDContext } from "../store/ContextStore";
 
 function JoinGameScreen() {
+    const [gameCode, setGameCode] = useState("");
+
+    const { session, databaseCheckGameId } = useContext(DDContext);
+
+    function isSixDigits(value) {
+        return /^\d{6}$/.test(value);
+      }
+
+    const joinGameHandler = () => {
+        if (isSixDigits(gameCode)) {
+            // Check if game code exists
+            if (databaseCheckGameId(gameCode)) {
+                // Join game
+                navigation.navigate("GameScreen", {
+                    dishes: newGameDishes,
+                    newGame: false,
+                });
+            } else {
+                alert("Game not found");
+            }
+        } else {
+            alert("Game code must be 6 digits");
+        }
+    }
+
     return (
         <View style={styles.root}>
             <Text>Join Game</Text>
+            <TextInput value={gameCode} onChangeText={setGameCode} placeholder="Enter Game Code" />
+            <Button title="Join Game" onPress={joinGameHandler} />
         </View>
     )
 }
