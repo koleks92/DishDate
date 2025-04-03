136d55946af81b10d39ae0cffad6a2b71d4533a1
Jan Konieczek <koleks92@gmail.com>
2025-04-03 22:19:17 +0200

    feat: Sizes constants

---
+++ constants/Sizes.js
@@ -0,0 +1,11 @@
+import { Dimensions } from "react-native";
+
+const scrH = Dimensions.get("screen").height;
+const scrW = Dimensions.get("screen").width;
+
+export const Sizes = {
+    scrH: scrH,
+    scrW: scrW,
+    
+    
+}
