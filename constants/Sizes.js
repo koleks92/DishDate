2b95c76a0be18402dc62beeceb419a7c92f2f442
Jan Konieczek <koleks92@gmail.com>
2025-04-07 10:39:57 +0200

    fix: Change to export default

--- constants/Sizes.js
+++ constants/Sizes.js
@@ -3,7 +3,7 @@ import { Dimensions } from "react-native";
 const scrH = Dimensions.get("screen").height;
 const scrW = Dimensions.get("screen").width;
 
-export const Sizes = {
+export default Sizes = {
     scrH: scrH,
     scrW: scrW,
     buttonInsidePadding: scrH * 0.015,
