59675f0f96bcfa04fa5b61a53a2948b9d216ffb5
Jan Konieczek <koleks92@gmail.com>
2025-04-10 18:22:18 +0200

    style: Comments, styling

--- constants/Sizes.js
+++ constants/Sizes.js
@@ -4,15 +4,28 @@ const scrH = Dimensions.get("screen").height;
 const scrW = Dimensions.get("screen").width;
 
 export default Sizes = {
+    // Screen sizes
     scrH: scrH,
     scrW: scrW,
+
+    // ButtonMain/ButtonLogo/InputField sizes
     buttonTextSize: scrH * 0.025,
-    inputTextSize: scrH * 0.02,
-    buttonLogoSize: scrH * 0.04,
     buttonWidth: scrW * 0.6,
     buttonHeight: scrW * 0.16,
     buttonMarginBottom: scrH * 0.015,
+
+    // ButtonLogo sizes
+    buttonLogoSize: scrH * 0.04,
+
+    // InputField sizes
+    inputTextSize: scrH * 0.02,
+
+    // Logo sizes
     logoSize: scrH * 0.4,
     logoContainerSize: scrH * 0.5,
+
+    // StartScreen sizes
     profileContainerHeight: scrH * 0.05
+
+    
 }
