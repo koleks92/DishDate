5d18603b40bd0c8096b954dc71c1176ffcacf6f8
Jan Konieczek <koleks92@gmail.com>
2025-04-24 20:50:35 +0200

    feat: red and green

--- constants/Colors.js
+++ constants/Colors.js
@@ -2,6 +2,7 @@ export default Colors = {
     background: "#f2de77",
     black: "#0a0a0a",
     white: "#F2F2F2",
-    backgroundButton: "#f2bc1b"
-
+    backgroundButton: "#f2bc1b",
+    green: "#558C03",
+    red: "#F24822"
 }
