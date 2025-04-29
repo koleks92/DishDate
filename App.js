5c2d03667e326829813bb2e0f34f87a834631e95
Jan Konieczek <koleks92@gmail.com>
2025-04-29 12:29:21 +0200

    fix: styling and cardStyle into backgroundColoru

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -19,8 +19,7 @@ import * as Notifications from "expo-notifications";
 
 import { SafeAreaView, StatusBar } from "react-native";
 import Colors from "./constants/Colors";
-import { useFonts, getLoadedFonts } from 'expo-font';
-
+import { useFonts, getLoadedFonts } from "expo-font";
 
 const Stack = createStackNavigator();
 
@@ -33,69 +32,73 @@ Notifications.setNotificationHandler({
 });
 
 function App() {
-
     const [loaded, error] = useFonts({
-        'Tektur-Regular': require('./assets/fonts/Tektur-Regular.ttf'),
-        'Tektur-Bold': require('./assets/fonts/Tektur-Bold.ttf')
-      });
+        "Tektur-Regular": require("./assets/fonts/Tektur-Regular.ttf"),
+        "Tektur-Bold": require("./assets/fonts/Tektur-Bold.ttf"),
+    });
 
     useEffect(() => {
-        StatusBar.setBarStyle('dark-content'); // Dark icons on status bar
+        StatusBar.setBarStyle("dark-content"); // Dark icons on status bar
         StatusBar.setBackgroundColor(Colors.background); // Makes status bar background transparent
     }, []);
 
     return (
         <SafeAreaProvider>
-            <SafeAreaView style={{ flex: 1, backgroundColor: Colors.background }}>
-            <DDProvider>
-                <NavigationContainer>
-                    <Stack.Navigator
-                        initialRouteName="StartScreen" // Set the initial screen here
-                        screenOptions={{ headerShown: false }}
-                    >
-                        <Stack.Screen
-                            name="StartScreen"
-                            component={StartScreen}
-                        />
-                        <Stack.Screen
-                            name="ProfileScreen"
-                            component={ProfileScreen}
-                        />
-                        <Stack.Screen
-                            name="NewGameScreen"
-                            component={NewGameScreen}
-                        />
-                        <Stack.Screen
-                            name="JoinGameScreen"
-                            component={JoinGameScreen}
-                        />
-                        <Stack.Screen
-                            name="DishesScreen"
-                            component={DishesScreen}
-                        />
-                        <Stack.Screen
-                            name="EditDishesScreen"
-                            component={EditDishesScreen}
-                        />
-                        <Stack.Screen
-                            name="DishesListScreen"
-                            component={DishesListScreen}
-                        />
-                        <Stack.Screen
-                            name="GameScreen"
-                            component={GameScreen}
-                        />
-                        <Stack.Screen
-                            name="GameResultsScreen"
-                            component={GameResultScreen}
-                        />
-                        <Stack.Screen
-                            name="GamesListScreen"
-                            component={GamesListScreen}
-                        />
-                    </Stack.Navigator>
-                </NavigationContainer>
-            </DDProvider>
+            <SafeAreaView
+                style={{ flex: 1, backgroundColor: Colors.background }}
+            >
+                <DDProvider>
+                    <NavigationContainer>
+                        <Stack.Navigator
+                            initialRouteName="StartScreen" // Set the initial screen here
+                            screenOptions={{
+                                headerShown: false,
+                                cardStyle: { backgroundColor: Colors.background }, // transition bg color
+                            }}
+                        >
+                            <Stack.Screen
+                                name="StartScreen"
+                                component={StartScreen}
+                            />
+                            <Stack.Screen
+                                name="ProfileScreen"
+                                component={ProfileScreen}
+                            />
+                            <Stack.Screen
+                                name="NewGameScreen"
+                                component={NewGameScreen}
+                            />
+                            <Stack.Screen
+                                name="JoinGameScreen"
+                                component={JoinGameScreen}
+                            />
+                            <Stack.Screen
+                                name="DishesScreen"
+                                component={DishesScreen}
+                            />
+                            <Stack.Screen
+                                name="EditDishesScreen"
+                                component={EditDishesScreen}
+                            />
+                            <Stack.Screen
+                                name="DishesListScreen"
+                                component={DishesListScreen}
+                            />
+                            <Stack.Screen
+                                name="GameScreen"
+                                component={GameScreen}
+                            />
+                            <Stack.Screen
+                                name="GameResultsScreen"
+                                component={GameResultScreen}
+                            />
+                            <Stack.Screen
+                                name="GamesListScreen"
+                                component={GamesListScreen}
+                            />
+                        </Stack.Navigator>
+                    </NavigationContainer>
+                </DDProvider>
             </SafeAreaView>
         </SafeAreaProvider>
     );
