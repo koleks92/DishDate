b4a60039245f8286477144e46af67354a38621bd
Jan Konieczek <koleks92@gmail.com>
2025-05-06 11:47:58 +0200

    feat: BackContainer component for goBack button

---
+++ components/UI/BackContainer.js
@@ -0,0 +1,39 @@
+import { StyleSheet, Pressable, View } from "react-native";
+import Sizes from "../../constants/Sizes";
+import ButtonLogo from "./ButtonLogo";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useNavigation } from "@react-navigation/native";
+
+function BackContainer() {
+    const navigation = useNavigation();
+
+    return (
+        <View style={styles.root}>
+            <Pressable
+                onPress={() => {
+                    navigation.goBack();
+                }}
+            >
+                <Ionicons
+                    name="chevron-back-outline"
+                    color={Colors.black}
+                    size={Sizes.backContainerHeight}
+                />
+            </Pressable>
+            
+            <></>
+        </View>
+    );
+}
+
+export default BackContainer;
+
+const styles = StyleSheet.create({
+    root: {
+        width: Sizes.scrW * 0.9,
+        height: Sizes.backContainerHeight,
+        alignContent: "center",
+        justifyContent: "space-between",
+        flexDirection: "row",
+    },
+});
