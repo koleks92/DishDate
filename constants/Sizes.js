f41f0512309db1269204d014e1a26a56531ab160
Jan Konieczek <koleks92@gmail.com>
2025-05-12 20:25:40 +0200

    feat: buttonInsidePadding

--- constants/Sizes.js
+++ constants/Sizes.js
@@ -13,6 +13,7 @@ export default Sizes = {
     buttonWidth: scrW * 0.6,
     buttonHeight: scrW * 0.16,
     buttonMarginBottom: scrH * 0.015,
+    buttonInsidePadding: scrH * 0.01,
 
     // ButtonLogo sizes
     buttonLogoSize: scrH * 0.04,
