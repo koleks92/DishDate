4949073c3fe78b7aad494c07382f2246bf143dc7
Jan Konieczek <koleks92@gmail.com>
2025-07-07 20:29:49 +0200

    fix: Moved from .env

--- util/supabase.js
+++ util/supabase.js
@@ -1,24 +1,24 @@
-import { AppState } from 'react-native'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createClient } from '@supabase/supabase-js'
+import { AppState } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL 
-const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
+const SUPABASE_URL = "https://qfrqpvxingpjmmebgkfh.supabase.co";
+const SUPABASE_ANON_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmcnFwdnhpbmdwam1tZWJna2ZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY0MTk1NDksImV4cCI6MjA1MTk5NTU0OX0.OGcnt0PwzqZg1lMZ3hXUn2XnhjEc0ufRI8W51I_clog";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     auth: {
-      storage: AsyncStorage,
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: false,
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
     },
-  });
+});
 
-
-AppState.addEventListener('change', (state) => {
-  if (state === 'active') {
-    supabase.auth.startAutoRefresh()
-  } else {
-    supabase.auth.stopAutoRefresh()
-  }
-})
+AppState.addEventListener("change", (state) => {
+    if (state === "active") {
+        supabase.auth.startAutoRefresh();
+    } else {
+        supabase.auth.stopAutoRefresh();
+    }
+});
