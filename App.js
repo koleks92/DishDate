71b4bdeffd0aeceda8608b38176d51d1804b22c1
Jan Konieczek <koleks92@gmail.com>
2025-08-06 20:56:40 +0200

    feat: Added blocked back navigation on iOS GameScreen

--- App.js
+++ App.js
@@ -28,22 +28,25 @@ import Colors from "./constants/Colors";
 import { useFonts, getLoadedFonts } from "expo-font";
 
 import * as Linking from "expo-linking";
-import * as Sentry from '@sentry/react-native';
+import * as Sentry from "@sentry/react-native";
 
 Sentry.init({
-  dsn: 'https://a76fafe33170682e6654aca54adae56b@o4509752951963648.ingest.de.sentry.io/4509753240715344',
+    dsn: "https://a76fafe33170682e6654aca54adae56b@o4509752951963648.ingest.de.sentry.io/4509753240715344",
 
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-  sendDefaultPii: true,
+    // Adds more context data to events (IP address, cookies, user, etc.)
+    // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+    sendDefaultPii: true,
 
-  // Configure Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+    // Configure Session Replay
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1,
+    integrations: [
+        Sentry.mobileReplayIntegration(),
+        Sentry.feedbackIntegration(),
+    ],
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: __DEV__,
+    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+    // spotlight: __DEV__,
 });
 
 const Stack = createStackNavigator();
@@ -152,6 +155,10 @@ function App() {
                             <Stack.Screen
                                 name="GameScreen"
                                 component={GameScreen}
+                                options={{
+                                    gestureEnabled: false, // disables swipe back on iOS
+                                    headerBackVisible: false, // hides the back arrow in header
+                                }}
                             />
                             <Stack.Screen
                                 name="GameResultsScreen"
