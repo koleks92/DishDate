54eeb768a2fa5d12a63fff1a424c2c6777790657
Jan Konieczek <koleks92@gmail.com>
2025-10-06 11:09:03 +0200

    feat: Custom checkbox component created

---
+++ components/UI/Checkbox.js
@@ -0,0 +1,73 @@
+import { StyleSheet, View, Text, Pressable } from "react-native";
+import Sizes from "../../constants/Sizes";
+import Colors from "../../constants/Colors";
+
+function Checkbox({ label, checked, onPress }) {
+    return (
+        <View style={styles.root}>
+            <View style={styles.shadow}>
+                <Pressable onPress={onPress}>
+                    <View style={styles.checkbox}>
+                        {checked ? <View style={styles.checkboxTrue} /> : <></>}
+                    </View>
+                </Pressable>
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.label}>{label}</Text>
+            </View>
+        </View>
+    );
+}
+
+export default Checkbox;
+
+const styles = StyleSheet.create({
+    root: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: Sizes.buttonMarginBottom,
+    },
+    shadow: {
+        backgroundColor: Colors.black,
+        transform: [
+            { translateX: 6 }, // Move horizontally
+            { translateY: 6 }, // Move vertically
+        ],
+    },
+    checkbox: {
+        width: Sizes.checkbox,
+        height: Sizes.checkbox,
+        borderWidth: 3,
+        borderColor: Colors.primary500,
+        backgroundColor: Colors.white,
+        transform: [
+            { translateX: -6 }, // Move horizontally
+            { translateY: -6 }, // Move vertically
+        ],
+    },
+    textContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    buttonPressed: {
+        transform: [
+            { scale: 0.95 },
+            { translateX: 6 }, // Move horizontally
+            { translateY: 6 }, // Move vertically]
+        ],
+    },
+    checkboxTrue: {
+        margin: "auto",
+        width: Sizes.checkbox - Sizes.checkbox / 3,
+        height: Sizes.checkbox - Sizes.checkbox / 3,
+        backgroundColor: Colors.black,
+    },
+    label: {
+        fontFamily: "Tektur-Bold",
+        color: Colors.black,
+        fontSize: Sizes.buttonTextSize,
+        textAlign: "center",
+    },
+});
